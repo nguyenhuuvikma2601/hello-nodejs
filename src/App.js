675924188db7f5889e8a11223f59b7d2c5ae9e52
nguyenhuuvikma2601 <nguyenhuuvikma2601@gmail.com>
2023-09-18 17:29:45 +0000

    commit ccode

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './ion.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h2>
+              GitHub Actions & Docker
+          </h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Cảm ơn các bạn
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
